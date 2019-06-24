885a189c7f3701de249fec42a884441de904cb1e
KorkyMonster <avivek3021@gmail.com>
2019-06-24 11:37:45 +0530

    Project setup

---
+++ lib/selenium_diepio_automation.rb
@@ -0,0 +1,6 @@
+require "selenium_diepio_automation/version"
+
+module SeleniumDiepioAutomation
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/selenium_diepio_automation_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe SeleniumDiepioAutomation do
+  it "has a version number" do
+    expect(SeleniumDiepioAutomation::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
